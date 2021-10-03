fcb7d338c5a65812b748c9ff842f761f0959b73f
lucifergotmad <amateur.fullstackdev@gmail.com>
2021-10-03 08:20:20 +0700

    add prestasi sekolah section

---
+++ frontend/components/profile-sekolah/StrukturOrganisasi.js
@@ -0,0 +1,51 @@
+import Container from "../Container";
+import SectionTitle from "../SectionTitle";
+
+export default function StrukturOrganisasi() {
+  return (
+    <section className="py-14">
+      <Container>
+        <SectionTitle title="Struktur Organisasi" />
+        <div className="text-xl flex flex-col items-center text-center py-14 px-14">
+          <div className="w-10/12 xl:w-2/12 md:w-3/12 mb-8">
+            <h3 className="font-bold mb-4">Kepala Sekolah</h3>
+            <img
+              src="paola-aguilar-LkQHGSVzfsk-unsplash.jpg"
+              className="h-80 w-full object-cover rounded-md"
+            />
+          </div>
+          <div className="w-full md:w-9/12 flex flex-col md:flex-row items-center justify-between space-y-8 md:space-y-0 md:space-x-8 font-semibold my-4">
+            <div className="w-10/12 md:w-3/12">
+              <h3 className="mb-4">Wakasek Kurikulum</h3>
+              <img
+                src="alexander-krivitskiy-awXBjFhh7iw-unsplash.jpg"
+                className="h-80 w-full object-cover rounded-md"
+              />
+            </div>
+            <div className="w-10/12 md:w-3/12">
+              <h3 className="mb-4">Wakasek Kesiswaan</h3>
+              <img
+                src="mateus-campos-felipe-rid_Zs9boQk-unsplash.jpg"
+                className="h-80 w-full object-cover rounded-md"
+              />
+            </div>
+            <div className="w-10/12 md:w-3/12">
+              <h3 className="mb-4">Wakasek Sarpras</h3>
+              <img
+                src="metin-ozer-iSmTwuKTNDo-unsplash.jpg"
+                className="h-80 w-full object-cover rounded-md"
+              />
+            </div>
+            <div className="w-10/12 md:w-3/12">
+              <h3 className="mb-4">Wakasek Hubinmas</h3>
+              <img
+                src="ellen-carlson-hanse-BibFuRgcO54-unsplash.jpg"
+                className="h-80 w-full object-cover rounded-md"
+              />
+            </div>
+          </div>
+        </div>
+      </Container>
+    </section>
+  );
+}
--- frontend/pages/profile-sekolah.js
+++ frontend/pages/profile-sekolah.js
@@ -1,9 +1,11 @@
 import Head from "next/head";
 import Link from "next/link";
+import { FaTrophy } from "react-icons/fa";
 import Container from "../components/Container";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import SejarahSingkat from "../components/profile-sekolah/SejarahSingkat";
+import StrukturOrganisasi from "../components/profile-sekolah/StrukturOrganisasi";
 import VisiMisi from "../components/profile-sekolah/VisiMisi";
 import SectionTitle from "../components/SectionTitle";
 
@@ -17,47 +19,32 @@ export default function ProfileSekolah() {
       <main>
         <SejarahSingkat />
         <VisiMisi />
-        <section className="py-14">
+        <StrukturOrganisasi />
+        <section className="py-14 bg-gray-100">
           <Container>
-            <SectionTitle title="Struktur Organisasi" />
-            <div className="text-xl flex flex-col items-center text-center py-14 px-14">
-              <div className="w-10/12 xl:w-2/12 md:w-3/12 mb-8">
-                <h3 className="font-bold mb-4">Kepala Sekolah</h3>
-                <img
-                  src="paola-aguilar-LkQHGSVzfsk-unsplash.jpg"
-                  className="h-80 w-full object-cover rounded-md"
-                />
-              </div>
-              <div className="w-full md:w-9/12 flex flex-col md:flex-row items-center justify-between space-y-8 md:space-y-0 md:space-x-8 font-semibold my-4">
-                <div className="w-10/12 md:w-3/12">
-                  <h3 className="mb-4">Wakasek Kurikulum</h3>
-                  <img
-                    src="alexander-krivitskiy-awXBjFhh7iw-unsplash.jpg"
-                    className="h-80 w-full object-cover rounded-md"
-                  />
+            <SectionTitle title="Prestasi Sekolah" />
+            <div className="px-4 md:px-0">
+              <div className="flex flex-wrap py-4">
+                <div className="w-full md:w-5/12 mt-2 flex space-x-4 items-center font-semibold bg-gray-900 text-gray-50 p-4 rounded-md md:mr-2">
+                  <FaTrophy />
+                  <p> Juara 1 Lomba Membaca</p>
                 </div>
-                <div className="w-10/12 md:w-3/12">
-                  <h3 className="mb-4">Wakasek Kesiswaan</h3>
-                  <img
-                    src="mateus-campos-felipe-rid_Zs9boQk-unsplash.jpg"
-                    className="h-80 w-full object-cover rounded-md"
-                  />
+                <div className="w-full md:w-5/12 mt-2 flex space-x-4 items-center font-semibold bg-gray-900 text-gray-50 p-4 rounded-md md:ml-2">
+                  <FaTrophy />
+                  <p> Juara 1 Lomba Membaca</p>
                 </div>
-                <div className="w-10/12 md:w-3/12">
-                  <h3 className="mb-4">Wakasek Sarpras</h3>
-                  <img
-                    src="metin-ozer-iSmTwuKTNDo-unsplash.jpg"
-                    className="h-80 w-full object-cover rounded-md"
-                  />
+                <div className="w-full md:w-5/12 mt-2 flex space-x-4 items-center font-semibold bg-gray-900 text-gray-50 p-4 rounded-md md:mr-2">
+                  <FaTrophy />
+                  <p> Juara 1 Lomba Membaca</p>
                 </div>
-                <div className="w-10/12 md:w-3/12">
-                  <h3 className="mb-4">Wakasek Hubinmas</h3>
-                  <img
-                    src="ellen-carlson-hanse-BibFuRgcO54-unsplash.jpg"
-                    className="h-80 w-full object-cover rounded-md"
-                  />
+                <div className="w-full md:w-5/12 mt-2 flex space-x-4 items-center font-semibold bg-gray-900 text-gray-50 p-4 rounded-md md:ml-2">
+                  <FaTrophy />
+                  <p> Juara 1 Lomba Membaca</p>
                 </div>
               </div>
+              <button className="block md:inline-block md:px-8 w-full md:w-auto bg-yellow-300 transition-all py-4 text-gray-50 text-lg rounded-md">
+                <Link href="/artikel">Lihat Berita</Link>
+              </button>
             </div>
           </Container>
         </section>
